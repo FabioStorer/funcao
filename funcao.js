a1c29be442c1ffd26059082b19e476fe6e4c36e1
Fabio.Storer <fabiostorer19@gmail.com>
2024-07-01 18:46:08 -0300

    feat funcao

---
+++ funcao.js
@@ -0,0 +1,12 @@
+let numero1 = 10;
+let numero2 = 20;
+
+let resultado = numero1 + numero2;
+
+function soma(numero1, numero2) {
+    let resultado = numero1 + numero2;
+    console.log('Resultado: ', resultado);
+    return resultado;
+}
+
+soma(3, 5);
